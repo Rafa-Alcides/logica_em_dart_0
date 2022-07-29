4db4c3eb4677e5d236ec1e40be7fe3e9f65f9c9e
Rafael Alcides <rafalcides@icloud.com>
2022-07-29 12:17:00 -0300

    Atividade 1 de Logica em dart_1

---
+++ android/app/src/main/kotlin/com/example/logica_em_dart_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.logica_em_dart_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Calculadora_de_Imc/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  const peso = 80;
+  const altura = 1.70;
+
+  const imc = peso / (altura * altura);
+  // ignore: avoid_print
+  print(imc);
+}
